5135b0c4361c360dbb02543699329ed550c978b1
agbekponou <alfredkofi17@gmail.com>
2023-06-16 23:46:19 +0000

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,10 +4,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
 /**
  * main - Entry point
  *
- * Return : Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {

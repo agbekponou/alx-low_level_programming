503dd700a7c1c69ad687100a1e649ed16d089e72
agbekponou <alfredkofi17@gmail.com>
2023-06-17 09:25:38 +0000

    Printing Combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print 2 combinations
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 48; i < 58; i++)
+{
+for (j = i + 1; j < 58; j++)
+{
+putchar(i);
+putchar(j);
+if (i !=56 || j != 57)
+{
+putchar(44);
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}

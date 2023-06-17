7b8823417e2f410e1b62f4a3dd09b0141e966e1f
agbekponou <alfredkofi17@gmail.com>
2023-06-17 08:22:09 +0000

    Combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,13 @@ int main(void)
 int i;
 for (i = 48; i < 58; i++)
 {
+putchar(i);
 if (i < 57)
 {
-putchar(i);
 putchar(44);
 putchar(' ');
 }
-else
-{
-putchar(i);
 }
-}
+putchar('\n');
 return (0);
 }
